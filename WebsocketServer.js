500cbf8cfbeeba5efca65844c4fd4bb01dc91ac4
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2020-08-25 21:08:33 -0300

    Make the client and server comunicate

--- WebsocketServer.js
+++ WebsocketServer.js
@@ -9,4 +9,14 @@ const wss = new websocket.Server({
   server,
 });
 
+wss.on('headers', (headers, req) => {
+  console.log(headers);
+  //websocket handshaking
+});
+
+wss.on('connection', (ws, req) => {
+  ws.send('Connection OK');
+  ws.on('message', (msg) => [console.log(msg)]);
+});
+
 server.listen(5000);
